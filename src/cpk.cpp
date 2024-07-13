6fb6784eb9268091f0d792adaef5bdf091290fcd
mos9527 <greats3an@gmail.com>
2024-07-13 18:38:34 +0800

    Make UTFTable RAII

--- src/cpk.cpp
+++ src/cpk.cpp
@@ -1,7 +1,9 @@
 // https://github.com/blueskythlikesclouds/MikuMikuLibrary/blob/master/MikuMikuLibrary/Archives/CriMw/CpkArchive.cs
 constexpr uint32_t CPK_MAGIC = fourCCBig('C', 'P', 'K', ' ');
 constexpr uint32_t UTF_MAGIC = fourCCBig('@', 'U', 'T', 'F');
+constexpr uint32_t CPK_OFFSET = 8;
 namespace cpk {
+	typedef std::variant<uint8_t, int8_t, uint16_t, int16_t, uint32_t, int32_t, uint64_t, int64_t, float, double, std::string, u8vec> utf_table_field_type;	
 	static void apply_utf_table_data_mask(u8vec& data) {
 		for (int i = 0, j = 25951; i < data.size(); i++, j *= 16661) data[i] ^= (j & 0xFF);
 	}
@@ -10,74 +12,84 @@ namespace cpk {
 		uint32_t _pad;
 		uint32_t length;
 		uint32_t _pad2;
-	};	
-	typedef std::variant<uint8_t, int8_t, uint16_t, int16_t, uint32_t, int32_t, uint64_t, int64_t, float, double, std::string, u8vec> utf_table_field_type;	
+	};
+	struct utf_table_sub_header {
+		uint32_t magic;
+		uint32_t length;
+		uint16_t encoding;
+		uint16_t rowOffset;
+		uint32_t stringPoolOffset;
+		uint32_t dataPoolOffset;
+		uint32_t nameOffset;
+		uint16_t fieldCount;
+		uint16_t rowStride;
+		uint32_t rowCount;		
+	};
 	struct utf_table_field {
-		char type;
+		uint8_t type;
 		std::string name;
 		bool hasDefaultValue;
 		utf_table_field_type defaultValue;
 		bool isValid;
-	};		
-	static void utf_table_data_from_vec(u8vec& data) {
-		apply_utf_table_data_mask(data);
-		u8stream stream(data, true);
-		uint32_t magic; stream.read(magic);
-		uint32_t length; stream.read(length);
-		uint16_t encoding; stream.read(encoding);
-		uint16_t rowOffset; stream.read(rowOffset);
-		uint32_t stringPoolOffset; stream.read(stringPoolOffset);
-		uint32_t dataPoolOffset; stream.read(dataPoolOffset);
-		uint32_t nameOffset; stream.read(nameOffset);
-		uint16_t fieldCount; stream.read(fieldCount);
-		uint16_t rowStride; stream.read(rowStride);
-		uint32_t rowCount; stream.read(rowCount);
-		CHECK(magic == UTF_MAGIC);
-		for (int i = 0; i < fieldCount; i++) {
-			char flags = stream.read<char>();
-			utf_table_field field;
-			field.type = flags & 0xF;
-			field.name = (flags & 0x10) ? stream.read<std::string>() : "";
-			field.hasDefaultValue = (flags & 0x20) != 0;
-			if (field.hasDefaultValue) {
-				switch (field.type) {
-				case 0: field.defaultValue = stream.read<uint8_t>(); break;
-				case 1: field.defaultValue = stream.read<int8_t>(); break;
-				case 2: field.defaultValue = stream.read<uint16_t>(); break;
-				case 3: field.defaultValue = stream.read<int16_t>(); break;
-				case 4: field.defaultValue = stream.read<uint32_t>(); break;
-				case 5: field.defaultValue = stream.read<int32_t>(); break;
-				case 6: field.defaultValue = stream.read<uint64_t>(); break;
-				case 7: field.defaultValue = stream.read<int64_t>(); break;
-				case 8: field.defaultValue = stream.read<float>(); break;
-				case 9: field.defaultValue = stream.read<double>(); break;
-				case 0xA: field.defaultValue = stream.read<std::string>(); break;
-				case 0xB:
-				{
-					u8vec buffer;
-					uint32_t offset = stream.read<uint32_t>();
-					uint32_t length = stream.read<uint32_t>();
-					size_t pos = stream.tell();
-					stream.seek(offset);
-					buffer.resize(length);
-					stream.read(buffer.data(), length);
-					stream.seek(pos);
-					field.defaultValue = buffer;
-				}
-				}
-				field.isValid = (flags & 0x40) != 0;
-			}
+	};
+	struct utf_table_stream : public u8stream {
+	private:
+		std::span<uint8_t> get_null_string_data(uint32_t offset) {
+			uint32_t pos = header.stringPoolOffset + 8 + offset; offset = pos;
+			while (src[pos]) pos++;
+			return { src.begin() + offset, src.begin() + pos };
 		}
-		return;
-	}
+		std::span<uint8_t> get_data_array_data(uint32_t offset, uint32_t length) {
+			uint32_t pos = header.dataPoolOffset + 8 + offset; offset = pos;
+			pos += length;
+			return { src.begin() + offset, src.begin() + pos };
+		}
+	public:
+		utf_table_sub_header header;
+		utf_table_stream(u8vec& data) : u8stream(data, true) {
+			*this >> header.magic >> header.length;
+			CHECK(header.magic == UTF_MAGIC);
+			*this >> header.encoding >> header.rowOffset >> header.stringPoolOffset >> header.dataPoolOffset >> header.nameOffset >> header.fieldCount >> header.rowStride >> header.rowCount;			
+		}
+		std::string read_null_string() {
+			uint32_t offset; *this >> offset;
+			auto sp = get_null_string_data(offset);
+			return { sp.begin(), sp.end() };
+		}
+		u8vec read_data_array() {
+			uint32_t offset, length; *this >> offset >> length;
+			auto sp = get_data_array_data(offset, length);
+			return { sp.begin(), sp.end() };
+		}
+		utf_table_field_type read_variant(uint32_t type) {
+			switch (type) {
+			case 0: return read<uint8_t>(); break;
+			case 1: return read<int8_t>(); break;
+			case 2: return read<uint16_t>(); break;
+			case 3: return read<int16_t>(); break;
+			case 4: return read<uint32_t>(); break;
+			case 5: return read<int32_t>(); break;
+			case 6: return read<uint64_t>(); break;
+			case 7: return read<int64_t>(); break;
+			case 8: return read<float>(); break;
+			case 9: return read<double>(); break;
+			case 0xA: return read_null_string(); break;
+			case 0xB: return read_data_array(); break;
+			default:
+				return 0;
+			};
+		}
+	};		
 
 	struct utf_table {
-		utf_table_header hdr{};		
+	public:
+		utf_table_header hdr;
+		std::unique_ptr<utf_table_stream> p_stream;
 		utf_table(FILE* fp) {
 			fread(&hdr, sizeof(hdr), 1, fp);
-			u8vec buffer(hdr.length);
-			fread(buffer.data(), 1, hdr.length, fp);
-			utf_table_data_from_vec(buffer);
+			u8vec buffer(hdr.length); fread(buffer.data(), 1, hdr.length, fp);
+			apply_utf_table_data_mask(buffer);	
+			p_stream.reset(new utf_table_stream(buffer));
 		}
 	};
 }
--- src/pch.hpp
+++ src/pch.hpp
@@ -24,48 +24,45 @@ constexpr uint32_t fourCC(const char a, const char b, const char c, const char d
 consteval uint32_t fourCCBig(const char a, const char b, const char c, const char d) {
 	return (a << 24) | (b << 16) | (c << 8) | (d << 0);
 };
+template<typename T> concept Fundamental = std::is_fundamental_v<T>;
 typedef std::vector<uint8_t> u8vec;
 struct u8stream {
 	u8vec& src;
 	size_t pos;
 	bool big_endian;
-	u8stream(u8vec& src, bool is_big_endian) : src(src), pos(0), big_endian(is_big_endian) {}	
+	u8stream(u8vec& src, bool is_big_endian) : src(src), pos(0), big_endian(is_big_endian) {}		
 	inline size_t tell() const {
 		return pos;
 	}
 	inline void seek(size_t pos) {
-		this->pos = pos;
+		pos = pos;
+	}
+	inline void read_at(void* dst, size_t size, size_t offset) {
+		CHECK(offset + size <= src.size(), "read out of bounds");
+		memcpy(dst, src.data() + offset, size);
+		// We assume the code runs on a little-endian machine
+		if (big_endian && size > 1) std::reverse((uint8_t*)dst, (uint8_t*)dst + size);		
 	}
 	inline void read(void* dst, size_t size) {
-		CHECK(pos + size <= src.size(), "read out of bounds");
-		memcpy(dst, src.data() + pos, size);
-		// We assume the code runs on a little-endian machine
-		if (big_endian) std::reverse((uint8_t*)dst, (uint8_t*)dst + size);
-		pos += size;
-	}
-	template<typename T> inline void read(T& dst) requires std::is_fundamental_v<T> {
-		read(&dst, sizeof(T));
+		read_at(dst, size, tell());
+		pos += size;		
+	}	
+	template<Fundamental T> inline void read_at(T& dst, size_t offset) {
+		read_at(&dst, sizeof(T), offset);
 	};
-	template<typename T> inline T read() requires std::is_fundamental_v<T> {
+	template<Fundamental T> inline void read(T& dst) {
+		read(&dst, sizeof(T));
+		return;
+	};
+	template<Fundamental T> inline T read_at(size_t offset) {
 		T dst;
-		read(dst);
+		read_at(dst, offset);
 		return dst;
 	}
-	template<typename T> inline std::string read() requires std::is_same_v<T, std::string> {
-		std::string dst;
-		while (src[pos]) {
-			dst.push_back(read<char>());
-		}
-		return dst;
-	}
-	inline void write(const void* src, size_t size) {
-		if (pos + size > this->src.size()) {
-			this->src.resize(pos + size);
-		}
-		memcpy(this->src.data() + pos, src, size);
-		pos += size;
-	}
-	template<typename T> inline void write(const T& src) requires std::is_fundamental_v<T> {
-		write(&src, sizeof(T));
-	}
-};
+	template<Fundamental T> inline T read() {
+		T ret = read_at<T>(pos);
+		pos += sizeof(T);
+		return ret;
+	}	
+	template<Fundamental T> inline u8stream& operator>>(T& value) { read(value); return *this; }
+}; 
